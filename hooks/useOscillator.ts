a1a18a8212503f3c57962a39f016f94d7a6cf49a
prdingilian <pr.dingilian@gmail.com>
2021-08-15 15:20:53 -0600

    move pusher and audio code into custom hooks

---
+++ hooks/useOscillator.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import { Voice, synthVoices } from "../constants/synth-presets";
+import { getFrequency } from "../web-audio/frequencies";
+import { audioContext } from "../web-audio/web-audio";
+
+const useOscillator = () => {
+  const [playOscillator, setPlayOscillator] =
+    useState<(noteValue: number, synthVoice: Voice) => void>();
+
+  useEffect(() => {
+    const { getOscillator } = audioContext(new AudioContext());
+    const oscillatorFn = (noteValue: number, synthVoice: Voice) => {
+      const frequency = getFrequency(noteValue);
+      const playOscillator = getOscillator(synthVoices[synthVoice], frequency);
+      playOscillator();
+    };
+    setPlayOscillator(() => oscillatorFn);
+  }, []);
+
+  return playOscillator;
+};
+
+export default useOscillator;
--- pages/api/pusher-auth.ts
+++ pages/api/pusher-auth.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import Pusher from "pusher";
+import { v4 as uuidv4 } from "uuid";
 
 const pusher = new Pusher({
   appId: process.env.PUSHER_ID!,
@@ -12,8 +13,8 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const socketId = req.body.socket_id;
   const channel = req.body.channel_name;
   const presenceData = {
-    user_id: Math.random().toString(),
-    user_info: { name: "hi" },
+    user_id: uuidv4(),
+    user_info: {},
   };
   const auth = pusher.authenticate(socketId, channel, presenceData);
   res.send(auth);
